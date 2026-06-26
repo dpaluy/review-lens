f7196f4bbcc7cf20ffb1f89c003fd30813a0f070
David Paluy <dpaluy@users.noreply.github.com>
2026-06-26 00:08:52 -0500

    Configure Active Storage for Spaces (#28)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # Store uploaded files in DigitalOcean Spaces via the S3-compatible adapter.
+  config.active_storage.service = :digitalocean_spaces
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
